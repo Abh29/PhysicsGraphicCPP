e9e9434be287acfc6543ea0f4e3315b8e225d525
Abh29 <aboutifor@stud.kpfu.ru>
2023-12-05 09:24:56 +0300

    updating ft_framebuffer ft_graphicspipeline

--- GraphicsViewer/FTGE/includes/ft_frameBuffer.h
+++ GraphicsViewer/FTGE/includes/ft_frameBuffer.h
@@ -6,8 +6,8 @@
 namespace ft {
 
 	class FrameBuffer {
-
-
+	public:
+		using pointer = std::shared_ptr<FrameBuffer>;
 
 	};
 
--- GraphicsViewer/FTGE/includes/ft_graphicsPipeline.h
+++ GraphicsViewer/FTGE/includes/ft_graphicsPipeline.h
@@ -7,6 +7,8 @@ namespace ft {
 
 class GraphicsPipeline {
 
+public:
+	using pointer = std::shared_ptr<GraphicsPipeline>;
 
 };
 }
